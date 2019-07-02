70c65541e11dc93d47d32ff89eb70cf59895550b
JarekParal <paral.jarek@gmail.com>
2019-07-02 16:42:49 +0200

    Make.bat: move file Make.bat from JarekParal/RB3rt-install to this repo
    - last commit in JarekParal/RB3rt-install:
      Make.bat: fix cygwin path, make the script more verbose

--- Make.bat
+++ Make.bat
@@ -1,5 +1,8 @@
-:: script for building EV3RT application
-:: author: Jaroslav Páral (jarekparal) 
+:: Script for building EV3RT application
+:: Author: Jaroslav Páral (jarekparal) 
+:: Version: 1.1 (2019-07-02)
+
+set "cygwin=C:\tools\cygwin"
 
 @IF "%1"=="" GOTO NO_PARAM
 @IF "%1"=="-b" GOTO BUILD
@@ -12,34 +15,33 @@ echo %1
 
 set "origin=%2%"
 
-@del /q C:\RB3rt\sdk\workspace\app
-@del /s/q C:\RB3rt\sdk\workspace\project\*
+del /q C:\RB3rt\sdk\workspace\app
+del /q C:\RB3rt\sdk\workspace\project\*
 
-@xcopy /s/y/q %origin%\* C:\RB3rt\sdk\workspace\project
+xcopy /y/q %origin%\* C:\RB3rt\sdk\workspace\project
 
-@IF "%1"=="-m" GOTO MAKE
+IF "%1"=="-m" GOTO MAKE
 
-@c:\cygwin64\bin\bash.exe -l -c " pwd ; cd /cygdrive/c/RB3rt/sdk/workspace ; pwd ; make app=project"
+%cygwin%\bin\bash.exe -l -c " pwd ; cd /cygdrive/c/RB3rt/sdk/workspace ; pwd ; make app=project"
 xcopy /y C:\RB3rt\sdk\workspace\app %origin%\.
-@echo Build time: %DATE% %TIME%
 
-@GOTO END
+GOTO END
 
 :MAKE
-@c:\cygwin64\bin\bash.exe -l -c " pwd ; cd /cygdrive/c/RB3rt/sdk/workspace ; pwd ; make app=project && make upload"
-@GOTO END
+%cygwin%\bin\bash.exe -l -c " pwd ; cd /cygdrive/c/RB3rt/sdk/workspace ; pwd ; make app=project && make upload"
+GOTO END
 
 
 :UPLOAD
-@c:\cygwin64\bin\bash.exe -l -c " pwd ; cd /cygdrive/c/RB3rt/sdk/workspace ; pwd ; make upload"
+%cygwin%\bin\bash.exe -l -c " pwd ; cd /cygdrive/c/RB3rt/sdk/workspace ; pwd ; make upload"
 
-@GOTO END
+GOTO END
 
 
 :NO_PARAM
-@echo "%0: no parameters"
-@GOTO END
+echo "%0: no parameters"
+GOTO END
 
 :WRONG_PARAM
-@echo "%0: wrong parameters"
+echo "%0: wrong parameters"
 :END
